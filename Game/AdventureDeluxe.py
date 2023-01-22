a2d6e0c5df7f13dc28e9fd1651e2c289b0a875d9
angelosoria <imangelosoria@gmail.com>
2023-01-22 13:28:12 +0800

    e

--- Game/AdventureDeluxe.py
+++ Game/AdventureDeluxe.py
@@ -35,9 +35,9 @@ def start_program():
     # Main brain for the game
     # Requires the parameter of logged-in account
     def adventure(logged_in_account):
-        # animations("game-title", True, .05) # Hidden
+        animations("game-title", True, .05) # Hidden
         clearscreen()
-        print("\nAdventure Deluxe! \n")
+        # print("\nAdventure Deluxe! \n")
         # Copy player stats
         thisPlayer = accounts[logged_in_account]
         # Plot
@@ -280,9 +280,9 @@ def start_program():
 
     # Main
     def startGame():
-        # animations("game-title", True, .105) # Hiddden
         clearscreen()
-        print("\nAdventure Deluxe!\n")
+        animations("game-title", True, .025) # Hiddden
+        # print("\nAdventure Deluxe!\n")
         print("═══════════════════════════════════════════════════════════")
         print("[1] Login")
         print("[2] Register")
